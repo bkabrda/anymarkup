73235f3490e773958736538dc18c8a2eee987ec2
Slavek Kabrda <bkabrda@redhat.com>
2015-06-16 14:23:11 +0200

    Bump version to 0.4.3

--- anymarkup/__init__.py
+++ anymarkup/__init__.py
@@ -12,7 +12,7 @@ import yaml
 
 
 __all__ = ['AnyMarkupError', 'parse', 'parse_file', 'serialize', 'serialize_file']
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 
 
 fmt_to_exts = {'ini': ['ini'], 'json': ['json'], 'xml': ['xml'], 'yaml': ['yaml', 'yml']}
