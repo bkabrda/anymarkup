0481ed6f5a85d4efffd2d2605d9c89897489f057
Slavek Kabrda <slavek.kabrda@datadoghq.com>
2019-07-25 09:09:36 +0200

    Release version 0.8.1

--- anymarkup/__init__.py
+++ anymarkup/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from anymarkup_core import AnyMarkupError, parse, parse_file, serialize, serialize_file
-__version__ = '0.8.0'
+__version__ = '0.8.1'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='anymarkup',
-    version='0.8.0',
+    version='0.8.1',
     description='Parse/serialize any markup format',
     long_description=''.join(open('README.rst').readlines()),
     keywords='xml, yaml, toml, json, json5, ini',
